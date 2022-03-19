3567796ce8ed7e8b16f762a7f5ee3fd32668ffd3
witchinthesky <maria.razymey@gmail.com>
2022-03-19 14:56:31 +0200

    delete unused fonts

--- app/src/main/java/com/example/hobbittracker/ui/theme/Color.kt
+++ app/src/main/java/com/example/hobbittracker/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.example.hobbittracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+var Orangish = Color(0xFFFDA758)
+var LightOrangeAccent = Color(255, 243, 233)
+var LightOrange = Color(255, 235, 217)
+var PurpleDark = Color(82, 51, 83)
+var PurpleLight = Color(171, 153, 169)
--- app/src/main/java/com/example/hobbittracker/ui/theme/Type.kt
+++ app/src/main/java/com/example/hobbittracker/ui/theme/Type.kt
@@ -2,17 +2,44 @@ package com.example.hobbittracker.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.hobbittracker.R
 
 // Set of Material typography styles to start with
+
+val headerFont = FontFamily(
+    Font(R.font.nunito_bold, FontWeight.Bold),
+    Font(R.font.nunito_light, FontWeight.Light),
+    Font(R.font.nunito_medium, FontWeight.Medium),
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_semibold, FontWeight.SemiBold),
+)
+
+val textFont = FontFamily(
+    Font(R.font.opensans_bold, FontWeight.Bold),
+    Font(R.font.opensans_light, FontWeight.Light),
+    Font(R.font.opensans_medium, FontWeight.Medium),
+    Font(R.font.opensans_regular, FontWeight.Normal),
+    Font(R.font.opensans_semibold, FontWeight.SemiBold),
+)
+
 val Typography = Typography(
+
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = textFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+
+    h1 = TextStyle(
+        fontFamily = headerFont,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 26.sp
     )
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
