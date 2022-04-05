19a4383b1e67caa0fa4c05aadcb44f2df94277ea
Erik <Gitarist.Erik@gmail.com>
2022-04-05 20:46:01 +0300

    Dependency injection for auth use cases

--- app/src/main/java/com/example/hobbittracker/di/DomainModule.kt
+++ app/src/main/java/com/example/hobbittracker/di/DomainModule.kt
@@ -1,6 +1,34 @@
 package com.example.hobbittracker.di
 
+import com.example.hobbittracker.domain.usecase.auth.*
 import org.koin.dsl.module
 
 val domainModule = module {
+    factory<LoginUseCase> {
+        LoginUseCase(authRepository = get())
+    }
+
+    factory<RegisterUseCase> {
+        RegisterUseCase(authRepository = get())
+    }
+
+    factory<SignInGoogleUseCase> {
+        SignInGoogleUseCase(authRepository = get())
+    }
+
+    factory<SignInFacebookUseCase> {
+        SignInFacebookUseCase(authRepository = get())
+    }
+
+    factory<LogoutUseCase> {
+        LogoutUseCase(authRepository = get())
+    }
+
+    factory<ResetPasswordUseCase> {
+        ResetPasswordUseCase(authRepository = get())
+    }
+
+    factory<CurrentUserUseCase> {
+        CurrentUserUseCase(authRepository = get())
+    }
 }
