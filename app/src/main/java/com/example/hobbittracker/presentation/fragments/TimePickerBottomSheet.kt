514660deb129f6233cb4fb9c929c46ecb8a84550
Erik <Gitarist.Erik@gmail.com>
2022-04-20 20:04:43 +0300

    change time picker

--- app/src/main/java/com/example/hobbittracker/presentation/fragments/TimePickerBottomSheet.kt
+++ app/src/main/java/com/example/hobbittracker/presentation/fragments/TimePickerBottomSheet.kt
@@ -1,8 +1,6 @@
 package com.example.hobbittracker.presentation.fragments
 
-import android.annotation.SuppressLint
 import android.content.DialogInterface
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,18 +9,21 @@ import androidx.fragment.app.FragmentManager
 import com.example.hobbittracker.R
 import com.example.hobbittracker.presentation.BaseBottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_sheet_time_picker.*
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
 
 class TimePickerBottomSheet(
     supportFragmentManager: FragmentManager
 ) : BaseBottomSheetDialogFragment(supportFragmentManager) {
 
     private var onSaveClickListener: ((
-        hours: Int,
-        minute: Int,
-        isAmSelected: Boolean
+        time: LocalTime
     ) -> Unit)? = null
 
     private var onCancelClickListener: (() -> Unit)? = null
+
+    private var isAmSelected: Boolean = true
 
     override fun getTheme(): Int {
         return R.style.bottomSheetDialogTheme
@@ -38,6 +39,7 @@ class TimePickerBottomSheet(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initTimePicker()
         setUpViews()
     }
 
@@ -50,37 +52,15 @@ class TimePickerBottomSheet(
             savePickedTime()
             dismiss()
         }
-
-        btn_am.setOnClickListener {
-            switchTimeMidday(isAmSelected = true)
-        }
-
-        btn_pm.setOnClickListener {
-            switchTimeMidday(isAmSelected = false)
-        }
-    }
-
-    @SuppressLint("ResourceAsColor")
-    private fun switchTimeMidday(isAmSelected: Boolean) {
-        if (isAmSelected) {
-            btn_am.setTextColor(R.color.purple_700)
-            btn_pm.setTextColor(R.color.orange_700)
-            btn_am.backgroundTintList = ColorStateList.valueOf(R.color.orange_700)
-            btn_pm.backgroundTintList = ColorStateList.valueOf(R.color.orange_100)
-        } else {
-            btn_am.setTextColor(R.color.orange_700)
-            btn_pm.setTextColor(R.color.purple_700)
-            btn_am.backgroundTintList = ColorStateList.valueOf(R.color.orange_100)
-            btn_pm.backgroundTintList = ColorStateList.valueOf(R.color.orange_700)
-        }
     }
 
     private fun savePickedTime() {
-        val hours = time_picker.getCurrentlySelectedTime("HH").toInt()
-        val minute = time_picker.getCurrentlySelectedTime("MM").toInt()
-        val isAmSelected = time_picker.getCurrentlySelectedTime("FORMAT") == "AM"
+        val time = LocalDateTime.ofInstant(
+            time_picker.date.toInstant(),
+            ZoneId.systemDefault()
+        ).toLocalTime()
 
-        onSaveClickListener?.let { it(hours, minute, isAmSelected) }
+        onSaveClickListener?.let { it(time) }
     }
 
     override fun onCancel(dialog: DialogInterface) {
@@ -88,14 +68,22 @@ class TimePickerBottomSheet(
         onCancelClickListener?.let { it() }
     }
 
+    private fun initTimePicker() {
+        time_picker.setDisplayMinutes(true)
+        time_picker.setDisplayHours(true)
+        time_picker.setDisplayDays(false)
+        time_picker.setDisplayMonths(false)
+        time_picker.setDisplayYears(false)
+        time_picker.setDisplayDaysOfMonth(false)
+        time_picker.setDisplayMonthNumbers(false)
+    }
+
     class Builder(
         private val fragmentManager: FragmentManager
     ) : BaseBottomSheetDialogFragment.Builder<TimePickerBottomSheet>() {
 
         private var onSaveListener: ((
-            hours: Int,
-            minute: Int,
-            isAmSelected: Boolean
+            time: LocalTime
         ) -> Unit)? = null
 
         private var onCancelListener: (() -> Unit)? = null
@@ -110,7 +98,7 @@ class TimePickerBottomSheet(
                 }
 
 
-        fun setOnSaveListener(callback: (hours: Int, minute: Int, isAmSelected: Boolean) -> Unit): Builder {
+        fun setOnSaveListener(callback: (time: LocalTime) -> Unit): Builder {
             onSaveListener = callback
             return this
         }
